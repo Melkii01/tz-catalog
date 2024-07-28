593a2ae6c0791dba63599d7112fd0c24ec9e0551
Lenar Mirzayanov <melkiy-dude01@yandex.ru>
2024-07-28 09:27:10 +0300

    get brands

--- data/app-back.js
+++ data/app-back.js
@@ -1,22 +1,33 @@
-import Brands from './brands.json';
-import Products from './products.json';
+import BrandsData from './brands.json';
+import ProductsData from './products.json';
 
 export class AppBack {
 
-    static getProducts(searchParams) {
-        if (Object.keys(searchParams).length !== 0) {
-
-            if (searchParams.page) {
-
-            }
-            if (searchParams.brands) {
-
-            }
-        }
-        return Products;
+    static getBrands(searchParams) {
+        // let brands = [];
+        //
+        // if (Object.keys(searchParams).length !== 0) {
+        //     if (searchParams.brands.length!==0){
+        //         searchParams.brands.forEach((brandUrl) => {
+        //
+        //         });
+        //     }
+        //
+        //
+        //
+        //     return brands;
+        // }
+        return BrandsData.filter(brand => brand.brand_id && brand.brand_name);
     }
 
-    static getBrands() {
-        return Brands;
+    static getProducts(){
+        return ProductsData.slice(0, 30);
     }
+
 }
+// brands.push(BrandsData.filter(brand => {
+//     if (Number(brand.brand_id) === Number(brandUrl)
+//         && brand.brand_id && brand.brand_name) {
+//         return brand;
+//     }
+// })[0]);
--- src/components/pages/catalog.js
+++ src/components/pages/catalog.js
@@ -1,6 +1,7 @@
 import '../../styles/pages/catalog.scss';
 import {UrlManager} from "../../utils/url-manager";
 import {AppBack} from "../../../data/app-back";
+import {Router} from "../../router";
 
 export class Catalog {
     constructor() {
@@ -19,11 +20,98 @@ export class Catalog {
             searchParams.brands = this.routeParams.brands.split(',');
             // console.log(searchParams.brands);
         }
-        AppBack.getProducts(searchParams);
-        this.showPage();
+        this.brands = AppBack.getBrands(searchParams);
+        // console.log(this.brands)
+        this.showBrands();
     }
 
-    showPage() {
+    showBrands() {
+        const brandsItemsElement = document.getElementById('brands-items');
+
+        this.brands.forEach((brand) => {
+            // console.log(brand)
+            if (brand && brand.brand_id && brand.brand_name) {
+                const brandElement = document.createElement('div');
+                brandElement.className = 'brands-item';
+
+                const brandLabelElement = document.createElement('label');
+                brandLabelElement.setAttribute("for", 'brand' + brand.brand_id);
+                brandElement.appendChild(brandLabelElement);
+
+                const brandInputElement = document.createElement('input');
+                brandInputElement.setAttribute("type", 'checkbox');
+                brandInputElement.setAttribute("id", 'brand' + brand.brand_id);
+
+                if (this.routeParams.brands) {
+                    this.routeParams.brands.split(',').find((item) => {
+                        if (brand.brand_id === item) {
+                            brandInputElement.setAttribute("checked", 'checked');
+                        }
+                    })
+                }
+                brandLabelElement.appendChild(brandInputElement);
+
+                const brandLabelTextElement = document.createElement('div');
+                brandLabelTextElement.innerText = brand.brand_name;
+                brandLabelElement.appendChild(brandLabelTextElement);
+
+                const that = this;
+                brandInputElement.addEventListener('change', function () {
+                    const checkedBrand = this.id.split('brand')[1];
+                    // console.log(that.routeParams.brands)
+
+                    // let checkedBrands = [...that.routeParams.brands];
+                    if (this.checked) {
+                        //     checkedBrands.push(checkedBrand)
+                        //     console.log(checkedBrands)
+                        console.log("Checkbox is checked..");
+                        // console.log(that.routeParams)
+                        // console.log(that.routeParams.brands)
+                        // if (checkedBrands.length >= 1) {
+                        // console.log(that.routeParams.brands)
+                        // that.routeParams.brands.split(',').push(checkedBrand)
+                        // console.log("push");
+                        // console.log(that.routeParams.brands)
+                        // let page = null;
+                        // if (that.routeParams.page) {
+                        //     page = that.routeParams.page;
+                        //
+                        //     location.href = '/' + '?page=' + page + '&brands=' + that.routeParams.brands.join(',');
+                        // }
+                        // location.href = '/' + '?brands=' + that.routeParams.brands.join(',');
+                        // } else {
+                        //
+                        // that.routeParams.brands = Array(checkedBrand);
+                        // console.log("create");
+                        // console.log(that.routeParams.brands)
+                        // // Router.activateRoute.bind(this)
+                        // let page = null;
+                        // if (that.routeParams.page) {
+                        //     page = that.routeParams.page;
+                        //     location.href = '/' + '?page=' + page + '&brands=' + that.routeParams.brands;
+                        // }
+                        // location.href = '/' + '?brands=' + that.routeParams.brands;
+
+                        // }
+
+                    } else {
+                        console.log("Checkbox is not checked..");
+                    }
+                });
+
+                brandsItemsElement.appendChild(brandElement);
+
+            }
+        })
+        this.showProducts();
+    }
+
+    showProducts() {
+        console.log(AppBack.getProducts());
+        AppBack.getProducts().forEach((product) => {
+
+        })
+
 
     }
 }
